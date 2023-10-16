35b56a9aaf1f99c7946fe64fa50924c201cd8f69
CloudxNess <pat.oniell@duocuc.cl>
2023-10-15 22:19:28 -0300

    Guards
    CanActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { PaginaprotegidaGuard } from './guards/paginaprotegida.guard';
 
 const routes: Routes = [
   {
@@ -21,7 +22,8 @@ const routes: Routes = [
   },
   {
     path: 'homes',
-    loadChildren: () => import('./homes/homes.module').then( m => m.HomesPageModule)
+    loadChildren: () => import('./homes/homes.module').then( m => m.HomesPageModule),
+    canActivate:[PaginaprotegidaGuard]
   },
   {
     path: 'registro-cliente',
