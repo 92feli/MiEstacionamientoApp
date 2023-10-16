7f3508f5bd1de2e3368a119d73d9ba05d8ee18be
CloudxNess <pat.oniell@duocuc.cl>
2023-10-16 12:54:16 -0300

    Capacitor Angular
    Instalacion de Capacitor Angular

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'MiEstacionamientoApp',
+  appId: 'www.miestacionamientoapp.com',
+  appName: 'app',
   webDir: 'www',
   server: {
     androidScheme: 'https'
