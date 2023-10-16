af6cef2df9d2b57cda69e5586fd9fcf7fb84c758
CloudxNess <pat.oniell@duocuc.cl>
2023-10-15 21:09:47 -0300

    Page not-found
    Modificacion de la Page not-found para redireccionar en caso de no encontrar la pagina correcta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,9 +42,15 @@ const routes: Routes = [
   {
     path: 'recuperar-contrasena',
     loadChildren: () => import('./pages/recuperar-contrasena/recuperar-contrasena.module').then( m => m.RecuperarContrasenaPageModule)
-  },  {
+  },
+  {
     path: 'not-found',
     loadChildren: () => import('./pages/not-found/not-found.module').then( m => m.NotFoundPageModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+    pathMatch: 'full'
   },
 
 
