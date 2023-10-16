d2d80d33bcedc362a9e0367ea7383683e17a2bcf
Matias <mat.romeros@duocuc.cl>
2023-10-16 13:19:11 -0300

    Firebase
    conexion a firebase + Login funcionando

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/authentication.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  Loginform: FormGroup
 
+  constructor(private toastController: ToastController, private alertController: AlertController, private loadingController: LoadingController,private authService: AuthenticationService, private router: Router, public formBuilder: FormBuilder) { }
   ngOnInit() {
+    this.Loginform = this.formBuilder.group({
+      email: [
+        ''
+      ],
+      password: [''
+      ],
+    });
   }
 
+  async login() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    // console.log(this.email + this.password);
+    if (this.Loginform.valid) {
+
+      //  await  loading.dismiss();
+      const user = await this.authService.LoginUser(this.Loginform.value.email, this.Loginform.value.password).catch((err) => {
+        this.presentToast(err)
+        console.log(err);
+        loading.dismiss();
+      })
+
+      if (user) {
+        loading.dismiss();
+        this.router.navigate(['/homes'])
+      }
+    } else {
+      return console.log('Please provide all the required values!');
+    }
+
+  }
+  get errorControl() {
+    return this.Loginform.controls;
+  }
+
+  async presentToast(message: undefined) {
+    console.log(message);
+
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 1500,
+      position: 'top',
+    });
+
+    await toast.present();
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'miestacionamiento-357de',
+    appId: '1:36501976762:web:2bd9eb9a34f716ff7db483',
+    storageBucket: 'miestacionamiento-357de.appspot.com',
+    apiKey: 'AIzaSyDVXN5OP06RKbWK___nKMmXIc_wGPS0-NE',
+    authDomain: 'miestacionamiento-357de.firebaseapp.com',
+    messagingSenderId: '36501976762',
+    measurementId: 'G-KY5G7GN5N1',
+  },
   production: true
 };
