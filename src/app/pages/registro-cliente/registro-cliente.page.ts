b5f2aef42dd7f19e5de1886274bbb25780076d72
Matias <mat.romeros@duocuc.cl>
2023-10-16 17:59:14 -0300

    Registro Cliente Firebase
    Registro Cliente Firebase

--- src/app/pages/registro-cliente/registro-cliente.page.ts
+++ src/app/pages/registro-cliente/registro-cliente.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/authentication.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro-cliente',
@@ -7,9 +12,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroClientePage implements OnInit {
 
-  constructor() { }
+  Regisform: FormGroup
 
-  ngOnInit() {
+  constructor(private toastController: ToastController,private alertController: AlertController,private loadingController: LoadingController,private authService: AuthenticationService, private router: Router, public formBuilder: FormBuilder) { 
+
   }
 
+  ngOnInit() {
+    // this.signUP()
+    this.Regisform = this.formBuilder.group({
+      fullname:[''
+    ],
+      email: [
+        '',
+      ],
+      password: ['', 
+    ],
+    });
+  }
+  get errorControl() {
+    return this.Regisform.controls;
+  }
+ 
+  async signUP(){
+    const loading = await this.loadingController.create();
+    await loading.present();
+    if (this.Regisform.valid) {
+
+      const user = await this.authService.registerUser(this.Regisform.value.email, this.Regisform.value.password).catch((err) => {
+        this.presentToast(err)
+        console.log(err);
+        loading.dismiss();
+      })
+
+      if (user) {
+        loading.dismiss();
+        this.router.navigate(['/homes'])
+      }
+    } else {
+      return console.log('Please provide all the required values!');
+    }
+  }
+  async presentToast(message: undefined) {
+    console.log(message);
+    
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 1500,
+      position: 'top',
+    });
+
+    await toast.present();
+  }
 }
