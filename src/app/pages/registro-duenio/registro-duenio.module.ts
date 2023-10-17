f25940fa4db38aee7903dd405db0c38cfca62947
Matias <mat.romeros@duocuc.cl>
2023-10-17 11:54:32 -0300

    Dueño Login
    Dueño Login a firebase auth

--- src/app/pages/registro-duenio/registro-duenio.module.ts
+++ src/app/pages/registro-duenio/registro-duenio.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,6 +13,7 @@ import { RegistroDuenioPage } from './registro-duenio.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     RegistroDuenioPageRoutingModule
   ],
   declarations: [RegistroDuenioPage]
