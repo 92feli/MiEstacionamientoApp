4a182d4d5bcb56e536a457bb892aebf12794e9f1
CloudxNess <pat.oniell@duocuc.cl>
2023-10-19 13:14:21 -0300

    Children
    Incorporacion de Children en la Pagina Homes

--- src/app/homes/homes-routing.module.ts
+++ src/app/homes/homes-routing.module.ts
@@ -1,12 +1,79 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { HomesPage } from './homes.page';
+import { LoginPage } from '../pages/login/login.page';
+import { DuenioHomePage } from '../pages/duenio-home/duenio-home.page';
+import { NotFoundPage } from '../pages/not-found/not-found.page';
+import { PruebaPage } from '../pages/prueba/prueba.page';
+import { RecuperarContrasenaPage } from '../pages/recuperar-contrasena/recuperar-contrasena.page';
+import { RegistroPage } from '../pages/registro/registro.page';
+import { RegistroClientePage } from '../pages/registro-cliente/registro-cliente.page';
+import { RegistroDuenioPage } from '../pages/registro-duenio/registro-duenio.page';
+import { SplashPage } from '../pages/splash/splash.page';
+import { TransaccionesPage } from '../pages/transacciones/transacciones.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomesPage
+    component: HomesPage,
+    children:[
+
+      {
+        path: 'duenio-home',
+        component: DuenioHomePage
+      },
+
+      {
+        path : 'login',
+        component: LoginPage
+      },
+
+      {
+        path: 'not-found',
+        component: NotFoundPage
+      },
+
+      {
+        path: 'prueba',
+        component: PruebaPage
+      },
+
+      {
+        path: 'recuperar-contrasena',
+        component: RecuperarContrasenaPage
+      },
+
+      {
+        path: 'registro',
+        component: RegistroPage
+      },
+
+      {
+        path: 'registro-cliente',
+        component: RegistroClientePage
+      },
+
+      {
+        path: 'registro-duenio',
+        component: RegistroDuenioPage
+      },
+
+      {
+        path: 'splash',
+        component: SplashPage
+      },
+
+      {
+        path: 'transacciones',
+        component: TransaccionesPage
+      },
+
+
+
+
+    ]
+
+
   }
 ];
 
