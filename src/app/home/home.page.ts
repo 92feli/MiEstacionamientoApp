b2d22a82288f384cc4920ecc96d73487a35595a3
CloudxNess <pat.oniell@duocuc.cl>
2023-10-20 12:29:52 -0300

    API
    Integracion de Google Maps

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild,AfterViewInit } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
+import { environment } from 'src/environments/environment';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  @ViewChild('map')
+  mapRef: ElementRef<HTMLElement>;
+  newMap: GoogleMap;
+  center : any = {
+    lat: -33.6016069,
+    lng: -70.5824042,
+  };
+
+
   constructor() {}
+
+  ngOnInit(){
+
+  }
+
+  ngAfterViewInit() {
+
+    this.createMap();
+
+  }
+  
+
+  async createMap() {
+    this.newMap = await GoogleMap.create({
+      id: 'capacitor-google-maps',
+      element: this.mapRef.nativeElement,
+      apiKey: environment.google_maps_api_key,
+      config: {
+        center: this.center,
+        zoom : 17,
+        
+      
+      },
+    });
+  }
+
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+
+  google_maps_api_key:'AIzaSyBBaNHRbT7fKEbMedFyXFTv1-RCE2Dzhu0',
+
+
+
   firebase: {
     projectId: 'miestacionamiento2',
     appId: '1:202563428078:web:9470a34e8427e5d8d15dff',
