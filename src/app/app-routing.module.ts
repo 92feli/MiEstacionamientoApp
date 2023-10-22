b7c1fd0685c09c549d9c248414e9418b9ab8b09c
CloudxNess <pat.oniell@duocuc.cl>
2023-10-21 23:05:24 -0300

    Pagina Protegida
    No podran acceder a las Pages sin logear

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { PaginaprotegidaGuard } from './guards/paginaprotegida.guard';
+import {AuthGuard}  from  '@angular/fire/auth-guard' ;
+
 
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canActivate:[AuthGuard]
+    
   },
   {
     path: '',
@@ -33,7 +37,7 @@ const routes: Routes = [
   {
     path: 'transacciones',
     loadChildren: () => import('./pages/transacciones/transacciones.module').then( m => m.TransaccionesPageModule),
-    canActivate:[PaginaprotegidaGuard]
+    canActivate:[AuthGuard]
   },
   {
     path: 'splash',
@@ -49,7 +53,8 @@ const routes: Routes = [
   },
   {
     path: 'duenio-home',
-    loadChildren: () => import('./pages/duenio-home/duenio-home.module').then( m => m.DuenioHomePageModule)
+    loadChildren: () => import('./pages/duenio-home/duenio-home.module').then( m => m.DuenioHomePageModule),
+    canActivate:[AuthGuard]
   },
 
   {
